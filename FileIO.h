da42972ac6f1e342720b0bde781b371e277e0130
Doublect <20724415+Doublect@users.noreply.github.com>
2021-02-04 15:56:25 +0100

    Adding CMake

---
+++ FileIO.h
@@ -0,0 +1,8 @@
+//
+// Created by doublect on 2021. 02. 03..
+//
+
+#ifndef COURSEWORK2_FILEIO_C_H
+#define COURSEWORK2_FILEIO_C_H
+
+#endif //COURSEWORK2_FILEIO_C_H
---
+++ Reader.h
@@ -0,0 +1,8 @@
+//
+// Created by doublect on 2021. 02. 03..
+//
+
+#ifndef COURSEWORK2_READER_H
+#define COURSEWORK2_READER_H
+
+#endif //COURSEWORK2_READER_H
